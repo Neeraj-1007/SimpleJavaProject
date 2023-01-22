0d060f9b50dd672ab0f826e2cdcc541f02bac576
Neeraj-1007 <neerajkumarjha10@gmail.com>
2023-01-22 15:55:07 +0530

    INit method removed and placed inside Main Method...comment all

--- src/main/java/com/simpleJavaProject/main/MainClass.java
+++ src/main/java/com/simpleJavaProject/main/MainClass.java
@@ -1,25 +1,27 @@
 package com.simpleJavaProject.main;
 
 
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
+//import java.util.logging.FileHandler;
+//import java.util.logging.Logger;
+//import java.util.logging.SimpleFormatter;
 
 public class MainClass {
 
     public static void main(String[] args) {
 
-         final Logger logger = Logger.getLogger("MainClass");
-        try {
-            FileHandler fh;
-            fh = new FileHandler("D:/GitWithJenkins/SimpleProject/logs/MyFile.log");
-            SimpleFormatter formatter=new SimpleFormatter();
-            fh.setFormatter(formatter);
-            logger.addHandler(fh);
-            logger.info("in init Method of Logger ");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        logger.info("in MAIN Method of Logger ");
+//         final Logger logger = Logger.getLogger("MainClass");
+//        try {
+//            FileHandler fh;
+//            fh = new FileHandler("D:/GitWithJenkins/SimpleProject/logs/MyFile.log");
+//            SimpleFormatter formatter=new SimpleFormatter();
+//            fh.setFormatter(formatter);
+//            logger.addHandler(fh);
+//            logger.info("in init Method of Logger ");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        logger.info("in MAIN Method of Logger ");
+
+        System.out.print("Hi");
     }
 }
